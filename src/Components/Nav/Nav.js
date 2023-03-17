2ebafef3b72b4eb5b23c41504b711701853f53cf
Mandy Tsang <mandy.tsang007@gmail.com>
2023-03-17 09:15:01 -0700

    Impliment navbar responsive feature

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Nav.css';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo-sea-turtle.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
+
 
 function Nav() {
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
+    const handleToggle = () => setIsNavExpanded(!isNavExpanded);
     return (
-        <nav className='navbar'>
-            <img src={logo} alt='Sea Turtle Logo' className='navbar-logo'/>
-            <ul className='categories'>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/">Home</Link></li>
-            </ul>
-        </nav>
+            <nav className='navbar'>
+                <img src={logo} alt='Sea Turtle Logo' className='navbar-logo' />
+                <button onClick={handleToggle} className="menu-icon">
+                    {isNavExpanded ? <FontAwesomeIcon icon={faX} /> : <FontAwesomeIcon icon={faBars} />
+                    }
+                </button>
+                <div className={isNavExpanded ? 'nav-menu expanded' : 'nav-menu'}>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/">Home</Link></li>
+                    </ul>
+                </div>
+            </nav>
     )
 }
 
