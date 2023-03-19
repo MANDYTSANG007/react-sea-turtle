fcc375e8821321a3d24aa7f8b83c21a8888e574a
Mandy Tsang <mandy.tsang007@gmail.com>
2023-03-18 20:17:53 -0700

    Add form input validation

--- src/Components/Booking/Booking.js
+++ src/Components/Booking/Booking.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./Booking.css";
 import BookingForm from "../BookingForm/BookingForm";
+import { fetchAPI, submitAPI } from "../../bookingAPI";
+
+export const initializeTimes = () => {
+    return { list: fetchAPI(new Date()) }
+};
+
+// Reducer
+export const updateTimes = (state, action) => 
+    action.date !== undefined ? { list: fetchAPI(new Date(action.date))} : state;
 
 function Booking() {
+    const initialState = initializeTimes();
+    const [availableTimes, setAvailableTimes] = useReducer(updateTimes, initialState);
     return (
         <section className="booking-section">
             <article className="booking-article">
                 <div className="booking-row">
                     <div className="column">
 
-                        <BookingForm />
+                        <BookingForm availableTimes={availableTimes} dispatch={setAvailableTimes} submit={submitAPI} />
 
                     </div>
                     <div className="column">
--- src/Components/BookingForm/BookingForm.js
+++ src/Components/BookingForm/BookingForm.js
@@ -1,22 +1,43 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./BookingForm.css";
 
-function BookingForm() {
+function BookingForm(props) {
     const [booking, setBooking] = useState({
         name: "",
+        email: "",
+        date: "",
+        time: "",
+        guests: "",
     })
     const [touched, setTouched] = useState({
         name: false,
+        email: false,
+        date: false,
+        time: false,
+        guests: false,
     })
     const [validations, setValidations] = useState({
         name: false,
+        email: false,
+        date: false,
+        time: false,
+        guests: false,
     })
+
     const [isSubmitted, setIsSubmitted] = useState(false);
+    const navigate = useNavigate();
+
     const handleSubmit = (e) => {
         e.preventDefault();
         if (Object.values(validations).some((value) => !value)) {
-            setTouched({ name: true, }
-            );
+            setTouched({
+                name: true,
+                email: true,
+                date: true,
+                time: true,
+                guests: true,
+            });
             return undefined;
         }
         const nameIsValid = booking.name.trim() !== '' && booking.name.length <= 200;
@@ -24,10 +45,39 @@ function BookingForm() {
             setTouched({ name: true });
             setValidations({ name: false });
             return;
-          }
+        }
         setIsSubmitted(true)
         console.log("submitted")
     }
+
+    const handleReset = () => {
+        setBooking({
+            name: "",
+            email: "",
+            date: "",
+            time: "",
+            guests: "",
+        });
+        setTouched({
+            name: false,
+            email: false,
+            date: false,
+            time: false,
+            guests: false,
+        });
+        setValidations({
+            name: false,
+            email: false,
+            date: false,
+            time: false,
+            guests: false,
+        });
+        setIsSubmitted(false);
+        navigate("/menu");
+    };
+
+    const emailValidation = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
+
     return (
         <>
             {!isSubmitted ? (
@@ -56,6 +106,118 @@ function BookingForm() {
                         }
                     </p>
 
+                    <p>
+                        <label htmlFor="email"> Email: </label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={booking.email}
+                            style={{ border: touched.email && !validations.email ? "solid red 1.5px" : "" }}
+                            required=""
+                            onChange={(e) => {
+                                setBooking({ ...booking, email: e.target.value });
+                                setValidations({ ...validations, email: emailValidation.test(e.target.value) })
+                            }}
+                            onBlur={() => setTouched({ ...touched, email: true })}
+                        />
+                        {
+                            <span style={{ display: touched.email && !validations.email ? "" : "none" }} >
+                                {"Please enter a valid email"}
+                            </span>
+                        }
+                    </p>
+
+                    <p>
+                        <label htmlFor="reservation-date"> Choose date </label>
+                        <input
+                            type="date"
+                            id="reservation-date"
+                            value={booking.date}
+                            required=""
+                            onChange={(e) => {
+                                props.dispatch({ date: e.target.value });
+                                setBooking({ ...booking, date: e.target.value });
+                                setValidations({
+                                    ...validations, date: new Date(booking.time
+                                        ? `${e.target.value} ${booking.time}`
+                                        : `${e.target.value} 00:00`
+                                    ).getTime() >= new Date().getTime()
+                                });
+                            }}
+                            style={{ border: touched.date && !validations.date ? "solid red 1.5px" : "" }}
+                            onBlur={() => setTouched({ ...touched, date: true })}
+                        />
+                        {<span style={{ display: touched.date && !validations.date ? "" : "none" }}>
+                            {booking.date ? "No reservation is avaliable in the past" : "Please choose a valid date"}
+                        </span>}
+                    </p>
+
+                    <p>
+                        <label htmlFor="reservation-time"> Choose time </label>
+                        <select
+                            id="reservation-time"
+                            value={booking.time}
+                            onChange={(e) => {
+                                setBooking({ ...booking, time: e.target.value });
+                                setValidations({
+                                    ...validations,
+                                    time: props.availableTimes.list.some(
+                                        (time) => time === e.target.value),
+                                    date: new Date(
+                                        e.target.value
+                                            ? `${booking.date} ${e.target.value}`
+                                            : `${booking.date} 00:00`
+                                    ).getTime() >= new Date().getTime(),
+                                });
+                            }}
+                            onBlur={() => { setTouched({ ...touched, time: true, }) }}
+                            style={{ border: touched.time && !validations.date ? "solid red 1.5px" : "", }}
+                        >
+                            <option />
+                            {props.availableTimes.list
+                                ? props.availableTimes.list.map((time) => {
+                                    return (
+                                        <option key={time} value={time}>
+                                            {time}
+                                        </option>
+                                    )
+                                })
+                                : ""}
+                        </select>
+                        {<span style={{
+                            display: touched.date && touched.time && !validations.date
+                                ? ""
+                                : "none"
+                        }}>
+                            {booking.time
+                                ? "You can't make a reservation in the past"
+                                : "Please choose a valid time"}
+                        </span>}
+                    </p>
+
+                    <p>
+                        <label htmlFor="guests"> Number of guests </label>
+                        <input
+                            type="number"
+                            id="guests"
+                            value={booking.guests}
+                            min="1"
+                            max="10"
+                            placeholder="Number between 1 to 10"
+                            onChange={(e) => {
+                                setBooking({...booking, guests: e.target.value})
+                                setValidations({...validations, guests: e.target.value > 0 && e.target.value <= 10});
+                            }}                                                        
+                            onBlur={() => {setTouched({...touched, guests: true})}}
+                            style={{ border: touched.guests && !validations.guests
+                                ? "solid red 1.5px"
+                                : ""
+                            }}
+                        />
+                        {<span style={{ display: touched.guests && !validations.guests ? "" : "none" }}>
+                            {"1-10"}
+                            </span>}
+                    </p>
                     <button
                         type="submit"
                         className="btn"
@@ -64,12 +226,16 @@ function BookingForm() {
                 </form>
             ) : (
                 <div>
-                    <p>Thank you {booking.name} for making a reservation. </p>
+                    <p>Thank you {booking.name} for making a reservation on {booking.date}. </p>
+                    <p> We are looking forward to seeing you soon! </p>
+                    <button className="reset-button" onClick={handleReset}> Check out our menu </button>
                 </div>
-            )}
+            )
+            }
         </>
     )
 
 }
 
 export default BookingForm;
+
